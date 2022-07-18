9809b111f3366eb1a6b0e6b1f84cd6a7dae291ee
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-18 14:55:17 -0500

    Add menu list, add transitons and animation smooth to the menu

--- js/index.js
+++ js/index.js
@@ -3,10 +3,30 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 const modal = document.querySelector("#menuModal");
 const hamburger = document.querySelector("#hamburger");
+const closeButton = document.querySelector("#closeButton");
+const listOption = document.querySelector("#menuList");
 
 console.log('javascript is working');
 
 hamburger.addEventListener('click', ()=>{
+    modal.classList.add("open-smooth");
+    modal.classList.remove("close-smooth"); 
     modal.style.display = "block";
 });
+
+closeButton.addEventListener('click', ()=>{
+    modal.classList.remove("open-smooth");   
+    modal.classList.add("close-smooth");
+
+    setTimeout (()=>{
+        modal.style.display = "none";
+    }
+    ,500);
+})
+
+listOption.addEventListener('click', ()=>{
+    modal.style.display = "none";
+})
+
+
 });
