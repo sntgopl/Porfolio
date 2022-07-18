16162bdc43cce51cc859f4fa61b831da6a981e10
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-07-18 17:45:33 -0300

    Correct linter errors

--- js/index.js
+++ js/index.js
@@ -1,32 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.querySelector('#menuModal');
+  const hamburger = document.querySelector('#hamburger');
+  const closeButton = document.querySelector('#closeButton');
+  const listOption = document.querySelector('#menuList');
 
-document.addEventListener('DOMContentLoaded', ()=>{
+  hamburger.addEventListener('click', () => {
+    modal.classList.add('open-smooth');
+    modal.classList.remove('close-smooth');
+    modal.style.display = 'block';
+  });
 
-const modal = document.querySelector("#menuModal");
-const hamburger = document.querySelector("#hamburger");
-const closeButton = document.querySelector("#closeButton");
-const listOption = document.querySelector("#menuList");
+  closeButton.addEventListener('click', () => {
+    modal.classList.remove('open-smooth');
+    modal.classList.add('close-smooth');
 
-console.log('javascript is working');
+    setTimeout(() => {
+      modal.style.display = 'none';
+    },
+    500);
+  });
 
-hamburger.addEventListener('click', ()=>{
-    modal.classList.add("open-smooth");
-    modal.classList.remove("close-smooth"); 
-    modal.style.display = "block";
-});
-
-closeButton.addEventListener('click', ()=>{
-    modal.classList.remove("open-smooth");   
-    modal.classList.add("close-smooth");
-
-    setTimeout (()=>{
-        modal.style.display = "none";
-    }
-    ,500);
-})
-
-listOption.addEventListener('click', ()=>{
-    modal.style.display = "none";
-})
-
-
+  listOption.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
 });
