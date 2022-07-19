aa46542bb05410afcbdf76a82b85db2433ff8da6
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-07-19 15:23:44 -0500

    Create jasonObject with the information

--- js/index.js
+++ js/index.js
@@ -8,6 +8,58 @@ document.addEventListener('DOMContentLoaded', () => {
   const projectButton1 = document.querySelector('#projectBtn1');
   const pupClose = document.querySelector('#popUpCloseButton');
 
+  const arrProjects = [
+    {'name': 'Project one',
+     'description': 'this is the project one',
+     'img':'images/projects/projects16',
+     'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+     'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+     'linkToSource': 'https://github.com/sntgopl/Porfolio'
+    },
+    {'name': 'Project Two',
+     'description': 'this is the project Two',
+     'img':'images/projects/projects16',
+     'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+     'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+     'linkToSource': 'https://github.com/sntgopl/Porfolio'
+    },
+    {'name': 'Project Three',
+    'description': 'this is the project Three',
+    'img':'images/projects/projects16',
+    'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+    'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+    'linkToSource': 'https://github.com/sntgopl/Porfolio'
+   },
+   {'name': 'Project Fourth',
+    'description': 'this is the project Fourth',
+    'img':'images/projects/projects16',
+    'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+    'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+    'linkToSource': 'https://github.com/sntgopl/Porfolio'
+  },
+  {'name': 'Project five',
+    'description': 'this is the project five',
+    'img':'images/projects/projects16',
+    'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+    'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+    'linkToSource': 'https://github.com/sntgopl/Porfolio'
+  },
+  {'name': 'Project six',
+   'description': 'this is the project six',
+   'img':'images/projects/projects16',
+   'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+   'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+   'linkToSource': 'https://github.com/sntgopl/Porfolio'
+  },
+  {'name': 'Project seven',
+   'description': 'this is the project seven',
+   'img':'images/projects/projects16',
+   'technologies':['html', 'Bootstrap', 'Ruby and Race'],
+   'linkToLive': 'https://github.com/sntgopl/sntgopl.github.io',
+   'linkToSource': 'https://github.com/sntgopl/Porfolio'
+  }    
+];
+
   hamburger.addEventListener('click', () => {
     modal.classList.add('open-smooth');
     modal.classList.remove('close-smooth');
